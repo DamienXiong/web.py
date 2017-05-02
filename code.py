73de05810c04ceaff25ee8a9341b4daf2934b86c
DamienXiong <2454471251@qq.com>
2017-05-02 16:49:03 +0800

    display information

--- code.py
+++ code.py
@@ -4,13 +4,24 @@ urls = (
     '/', 'index'
 )
 
+movies = [
+    {
+        'title': 'Forrest Gump',
+        'year': 1994,
+    },
+    {
+        'title': 'Titanic',
+        'year': 1997
+    },
+]
+
+render = web.template.render('templates/')
+
 class index:
     def GET(self):
-        return 'Hello, world!'
+        return render.index(movies)
 
 if __name__ == '__main__':
     app = web.application(urls, globals())
     app.run()
 
-#在命令行运行python code.py会输出http://0.0.0.0:8080/，firefox浏览器无法打开此地址，
-#运行python code.py 127.0.0.1,然后访问http://127.0.0.1:8080/
