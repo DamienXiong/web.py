8fab9c98dde8a0f0893845b10fe61cd4f875655d
DamienXiong <2454471251@qq.com>
2017-05-03 09:36:53 +0800

    添加超链接

--- code.py
+++ code.py
@@ -1,7 +1,8 @@
 import web
 
 urls = (
-    '/', 'index'
+    '/', 'index',
+    '/movie/(\d+)', 'movie',
 )
 
 db = web.database(dbn='sqlite', db='MovieSite.db')
@@ -12,6 +13,12 @@ class index:
     def GET(self):
         movies = db.select('movie')
         return render.index(movies)
+
+class movie:
+    def GET(self, movie_id):
+        movie_id = int(movie_id)
+        movie = db.select('movie', where='id=$movie_id', vars=locals())[0]
+        return render.movie(movie)
 
 if __name__ == '__main__':
     app = web.application(urls, globals())
